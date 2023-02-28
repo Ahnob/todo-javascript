326ce8dcb7bf91ae50917d06234b343393346db6
Anob Takon <anobtakon@gmail.com>
2023-02-28 16:53:06 +0000

    started adding javascript

---
+++ script.js
@@ -0,0 +1,38 @@
+// selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+// Event listeners
+todoButton.addEventListener("click", addTodo);
+
+// functions
+function addTodo(event) {
+  // preventdefault.
+  event.preventDefault();
+
+  // tododiv
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+
+  // createLI
+  const createTodo = document.createElement("Li");
+  createTodo.innerText = "hey babe";
+  createTodo.classList.add("todo-item");
+  todoDiv.appendChild(createTodo);
+
+  // Check mark button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class= "fa-regular fa-check></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+
+  // trash button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fa-regular fa-trash"></i>';
+  trashButton.classList.add("complete-btn");
+  todoDiv.appendChild(trashButton);
+
+  //    append to list
+  todoList.appendChild(todoDiv);
+}
